3b8e9f63feff2c7711e88b1540d072aaa8a78bcd
renang20 <142357077+renang20@users.noreply.github.com>
2024-06-17 20:18:54 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>
+        Eu estou arrasando na programação com expo.
+      </Text>
+      <Text style={styles.signature}>Ass.: Renan Gomes</Text>
     </View>
   );
 }
@@ -13,8 +15,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  signature: {
+    fontSize: 18,
+    textAlign: 'center',
   },
 });
